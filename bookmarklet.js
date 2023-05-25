1516764e98c6d257a3b152d7fb4c5a68d50d6ce1
znlysocial <134474004+znlysocial@users.noreply.github.com>
2023-05-25 16:15:20 -0400

    Reverted Update, Broke Stuff
    Triggered the " This document requires `TrustedHTL` assignment " that broke key binds.
    
    Signed-off-by: znlysocial <134474004+znlysocial@users.noreply.github.com>

--- bookmarklet.js
+++ bookmarklet.js
@@ -12,7 +12,6 @@ javascript:(function() {
       return;
     }
     var o = document.createElement('div');
-    o.id = 'drag-window';
     o.style.position = 'fixed';
     o.style.top = '0';
     o.style.left = '0';
@@ -48,7 +47,7 @@ javascript:(function() {
     title.style.fontWeight = 'normal';
     title.style.fontFamily = 'Poppins, sans-serif';
     title.style.fontSize = '14px';
-    title.textContent = 'Zenly PB v0.80';
+    title.textContent = 'Zenly';
     title.style.userSelect = 'none';
     w.appendChild(title);
     var cOrange = document.createElement('div');
@@ -172,22 +171,17 @@ javascript:(function() {
     window.addEventListener('touchmove', handleMove);
     window.addEventListener('touchend', handleEnd);
 
-    var script = document.createElement('script');
-    script.innerHTML = `
-      window.addEventListener('keydown', function(event) {
-        if (event.shiftKey && event.key === 'Z') {
-          var windows = document.querySelectorAll('#drag-window');
-          for (var i = 0; i < windows.length; i++) {
-            var window = windows[i];
-            window.style.opacity = '0';
-            window.style.transform = 'scale(0)';
-            setTimeout(function() {
-              window.parentNode.removeChild(window);
-            }, 300);
-          }
-        }
-      });
-    `;
-    document.body.appendChild(script);
+    var style = document.createElement('style');
+    style.innerHTML = '.drag-window{position:fixed;top:0;left:0;width:300px;height:180px;background-color:#1a1a1a;opacity:1;border-radius:12px;z-index:9999;}' +
+      '.drag-window-bar{position:absolute;top:0;left:0;width:100%;height:27px;background-color:#333;border-radius:12px 12px 0 0;cursor:move;}' +
+      '.drag-window-title{position:absolute;top:8px;left:50%;transform:translateX(-50%);color:white;font-weight:normal;font-family:Poppins,sans-serif;font-size:14px;user-select:none;}' +
+      '.drag-window-close{position:absolute;top:8px;right:26px;width:12px;height:12px;background-color:orange;border-radius:6px;cursor:pointer;}' +
+      '.drag-window-close::before{position:absolute;content:"";top:4px;left:2px;width:8px;height:4px;background-color:white;border-radius:2px;}' +
+      '.drag-window-close::after{position:absolute;content:"";top:2px;left:4px;width:4px;height:8px;background-color:white;border-radius:2px;}' +
+      '.drag-window-close:hover{background-color:red;}' +
+      '.drag-window-alert-button{position:absolute;top:60px;padding:10px;background-color:#383838;color:white;border:none;border-radius:4px;cursor:pointer;}' +
+      '.drag-window-alert-button:hover{background-color:#555;}' +
+      '.drag-window-alert-button:focus{outline:none;}';
+    document.head.appendChild(style);
   }
 })();
