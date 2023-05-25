505aefac7fa6ca9c6877669ec047733c0083afa8
znlysocial <134474004+znlysocial@users.noreply.github.com>
2023-05-25 10:18:39 -0400

    fixed it
    Signed-off-by: znlysocial <134474004+znlysocial@users.noreply.github.com>

--- temporary-test.js
+++ temporary-test.js
@@ -1,1 +1,149 @@
-javascript:(function() {  var link = document.createElement('link');  link.rel = 'stylesheet';  link.href = 'https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap%27;%20%20link.onload%20=%20function()%20{%20%20%20%20initializeBookmarklet();%20%20};%20%20document.head.appendChild(link);%20%20function%20initializeBookmarklet()%20{%20%20%20%20if%20(document.getElementById(%27drag-window%27))%20{%20%20%20%20%20%20return;%20%20%20%20}%20%20%20%20var%20o%20=%20document.createElement(%27div%27);%20%20%20%20o.style.position%20=%20%27fixed%27;%20%20%20%20o.style.top%20=%20%270%27;%20%20%20%20o.style.left%20=%20%270%27;%20%20%20%20o.style.width%20=%20%27300px%27;%20%20%20%20o.style.height%20=%20%27180px%27;%20%20%20%20o.style.backgroundColor%20=%20%27#1a1a1a';%20%20%20%20o.style.opacity%20=%20'0';%20%20%20%20o.style.borderRadius%20=%20'12px';%20%20%20%20o.style.zIndex%20=%20'9999';%20%20%20%20o.style.transform%20=%20'scale(0)';%20%20%20%20o.style.transition%20=%20'opacity%200.3s,%20transform%200.3s';%20%20%20%20document.body.appendChild(o);%20%20%20%20requestAnimationFrame(function()%20{%20%20%20%20%20%20o.style.opacity%20=%20'1';%20%20%20%20%20%20o.style.transform%20=%20'scale(1)';%20%20%20%20});%20%20%20%20var%20w%20=%20document.createElement('div');%20%20%20%20w.style.position%20=%20'absolute';%20%20%20%20w.style.top%20=%20'0';%20%20%20%20w.style.left%20=%20'0';%20%20%20%20w.style.width%20=%20'100%';%20%20%20%20w.style.height%20=%20'30px';%20%20%20%20w.style.backgroundColor%20=%20'#333333';%20%20%20%20w.style.borderRadius%20=%20'12px%2012px%200%200';%20%20%20%20w.style.cursor%20=%20'move';%20%20%20%20o.appendChild(w);%20%20%20%20var%20title%20=%20document.createElement('div');%20%20%20%20title.style.position%20=%20'absolute';%20%20%20%20title.style.top%20=%20'5px';%20%20%20%20title.style.left%20=%20'5px';%20%20%20%20title.style.color%20=%20'white';%20%20%20%20title.style.fontWeight%20=%20'normal';%20%20%20%20title.style.fontFamily%20=%20'Poppins,%20sans-serif';%20%20%20%20title.style.fontSize%20=%20'14px';%20%20%20%20title.textContent%20=%20'Zenly';%20%20%20%20title.style.userSelect%20=%20'none';%20%20%20%20w.appendChild(title);%20%20%20%20var%20cOrange%20=%20document.createElement('div');%20%20%20%20cOrange.style.position%20=%20'absolute';%20%20%20%20cOrange.style.top%20=%20'3px';%20%20%20%20cOrange.style.right%20=%20'25px';%20%20%20%20cOrange.style.width%20=%20'16px';%20%20%20%20cOrange.style.height%20=%20'16px';%20%20%20%20cOrange.style.backgroundColor%20=%20'orange';%20%20%20%20cOrange.style.borderRadius%20=%20'6px';%20%20%20%20cOrange.style.cursor%20=%20'pointer';%20%20%20%20w.appendChild(cOrange);%20%20%20%20var%20cRed%20=%20document.createElement('div');%20%20%20%20cRed.style.position%20=%20'absolute';%20%20%20%20cRed.style.top%20=%20'3px';%20%20%20%20cRed.style.right%20=%20'3px';%20%20%20%20cRed.style.width%20=%20'16px';%20%20%20%20cRed.style.height%20=%20'16px';%20%20%20%20cRed.style.backgroundColor%20=%20'red';%20%20%20%20cRed.style.borderRadius%20=%20'6px';%20%20%20%20cRed.style.cursor%20=%20'pointer';%20%20%20%20w.appendChild(cRed);%20%20%20%20%20%20%20%20var%20alertButton1%20=%20document.createElement('button');%20%20%20%20alertButton1.textContent%20=%20'Alert%201';%20%20%20%20alertButton1.style.position%20=%20'absolute';%20%20%20%20alertButton1.style.top%20=%20'60px';%20%20%20%20alertButton1.style.left%20=%20'30px';%20%20%20%20alertButton1.style.padding%20=%20'10px';%20%20%20%20alertButton1.style.backgroundColor%20=%20'blue';%20%20%20%20alertButton1.style.color%20=%20'white';%20%20%20%20alertButton1.style.border%20=%20'none';%20%20%20%20alertButton1.style.borderRadius%20=%20'4px';%20%20%20%20alertButton1.style.cursor%20=%20'pointer';%20%20%20%20o.appendChild(alertButton1);%20%20%20%20%20%20%20%20var%20alertButton2%20=%20document.createElement('button');%20%20%20%20alertButton2.textContent%20=%20'Alert%202';%20%20%20%20alertButton2.style.position%20=%20'absolute';%20%20%20%20alertButton2.style.top%20=%20'60px';%20%20%20%20alertButton2.style.left%20=%20'140px';%20%20%20%20alertButton2.style.padding%20=%20'10px';%20%20%20%20alertButton2.style.backgroundColor%20=%20'green';%20%20%20%20alertButton2.style.color%20=%20'white';%20%20%20%20alertButton2.style.border%20=%20'none';%20%20%20%20alertButton2.style.borderRadius%20=%20'4px';%20%20%20%20alertButton2.style.cursor%20=%20'pointer';%20%20%20%20o.appendChild(alertButton2);%20%20%20%20%20%20%20%20cOrange.addEventListener('click',%20function()%20{%20%20%20%20%20%20o.style.opacity%20=%20'0';%20%20%20%20%20%20o.style.transform%20=%20'scale(0)';%20%20%20%20%20%20setTimeout(function()%20{%20%20%20%20%20%20%20%20o.parentNode.removeChild(o);%20%20%20%20%20%20},%20300);%20%20%20%20});%20%20%20%20cRed.addEventListener('click',%20function()%20{%20%20%20%20%20%20o.style.opacity%20=%20'0';%20%20%20%20%20%20o.style.transform%20=%20'scale(0)';%20%20%20%20%20%20setTimeout(function()%20{%20%20%20%20%20%20%20%20o.parentNode.removeChild(o);%20%20%20%20%20%20},%20300);%20%20%20%20});%20%20%20%20%20%20%20%20alertButton1.addEventListener('click',%20function()%20{%20%20%20%20%20%20alert('Alert%201%20triggered!');%20%20%20%20});%20%20%20%20%20%20%20%20alertButton2.addEventListener('click',%20function()%20{%20%20%20%20%20%20alert('Alert%202%20triggered!');%20%20%20%20});%20%20%20%20%20%20%20%20var%20isDragging%20=%20false;%20%20%20%20var%20startX%20=%200;%20%20%20%20var%20startY%20=%200;%20%20%20%20var%20initialLeft%20=%200;%20%20%20%20var%20initialTop%20=%200;%20%20%20%20w.addEventListener('mousedown',%20function(e)%20{%20%20%20%20%20%20isDragging%20=%20true;%20%20%20%20%20%20startX%20=%20e.clientX;%20%20%20%20%20%20startY%20=%20e.clientY;%20%20%20%20%20%20initialLeft%20=%20parseFloat(o.style.left)%20||%200;%20%20%20%20%20%20initialTop%20=%20parseFloat(o.style.top)%20||%200;%20%20%20%20%20%20o.style.cursor%20=%20'move';%20%20%20%20});%20%20%20%20window.addEventListener('mousemove',%20function(e)%20{%20%20%20%20%20%20if%20(isDragging)%20{%20%20%20%20%20%20%20%20var%20offsetX%20=%20e.clientX%20-%20startX;%20%20%20%20%20%20%20%20var%20offsetY%20=%20e.clientY%20-%20startY;%20%20%20%20%20%20%20%20o.style.left%20=%20initialLeft%20+%20offsetX%20+%20'px';%20%20%20%20%20%20%20%20o.style.top%20=%20initialTop%20+%20offsetY%20+%20'px';%20%20%20%20%20%20}%20%20%20%20});%20%20%20%20window.addEventListener('mouseup',%20function()%20{%20%20%20%20%20%20isDragging%20=%20false;%20%20%20%20%20%20o.style.cursor%20=%20'default';%20%20%20%20});%20%20%20%20var%20style%20=%20document.createElement('style');%20%20%20%20style.innerHTML%20=%20'.drag-window{position:fixed;top:0;left:0;width:300px;height:180px;background-color:#1a1a1a;opacity:1;border-radius:12px;z-index:9999;}'%20+%20%20%20%20%20%20'.drag-window-bar{position:absolute;top:0;left:0;width:100%;height:30px;background-color:#333333;border-radius:12px%2012px%200%200;cursor:move;}'%20+%20%20%20%20%20%20'.drag-window-title{position:absolute;top:5px;left:5px;color:white;font-weight:normal;font-family:%22Poppins%22,sans-serif;font-size:14px;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none;}'%20+%20%20%20%20%20%20'.drag-window-close{position:absolute;top:3px;right:25px;width:16px;height:16px;background-color:orange;border-radius:6px;cursor:pointer;}'%20+%20%20%20%20%20%20'.drag-window-second-close{position:absolute;top:3px;right:3px;width:16px;height:16px;background-color:red;border-radius:6px;cursor:pointer;}';%20%20%20%20document.head.appendChild(style);%20%20%20%20o.classList.add('drag-window');%20%20%20%20w.classList.add('drag-window-bar');%20%20%20%20title.classList.add('drag-window-title');%20%20%20%20cOrange.classList.add('drag-window-close');%20%20%20%20cRed.classList.add('drag-window-second-close');%20%20}})();
+javascript: (function() {
+  var link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = 'https://fonts.googleapis.com/css2?family=Poppins:wght@400&display=swap';
+  link.onload = function() {
+    initializeBookmarklet();
+  };
+  document.head.appendChild(link);
+
+  function initializeBookmarklet() {
+    if (document.getElementById('drag-window')) {
+      return;
+    }
+    var o = document.createElement('div');
+    o.style.position = 'fixed';
+    o.style.top = '0';
+    o.style.left = '0';
+    o.style.width = '300px';
+    o.style.height = '180px';
+    o.style.backgroundColor = '#1a1a1a';
+    o.style.opacity = '0';
+    o.style.borderRadius = '12px';
+    o.style.zIndex = '9999';
+    o.style.transform = 'scale(0)';
+    o.style.transition = 'opacity 0.3s, transform 0.3s';
+    document.body.appendChild(o);
+    requestAnimationFrame(function() {
+      o.style.opacity = '1';
+      o.style.transform = 'scale(1)';
+    });
+
+    var w = document.createElement('div');
+    w.style.position = 'absolute';
+    w.style.top = '0';
+    w.style.left = '0';
+    w.style.width = '100%';
+    w.style.height = '30px';
+    w.style.backgroundColor = '#333333';
+    w.style.borderRadius = '12px 12px 0 0';
+    w.style.cursor = 'move';
+    o.appendChild(w);
+
+    var title = document.createElement('div');
+    title.style.position = 'absolute';
+    title.style.top = '5px';
+    title.style.left = '5px';
+    title.style.color = 'white';
+    title.style.fontWeight = 'normal';
+    title.style.fontFamily = 'Poppins, sans-serif';
+    title.style.fontSize = '14px';
+    title.textContent = 'Zenly';
+    title.style.userSelect = 'none';
+    w.appendChild(title);
+
+    var cOrange = document.createElement('div');
+    cOrange.style.position = 'absolute';
+    cOrange.style.top = '3px';
+    cOrange.style.right = '25px';
+    cOrange.style.width = '16px';
+    cOrange.style.height = '16px';
+    cOrange.style.backgroundColor = 'orange';
+    cOrange.style.borderRadius = '6px';
+    cOrange.style.cursor = 'pointer';
+    w.appendChild(cOrange);
+
+    var cRed = document.createElement('div');
+    cRed.style.position = 'absolute';
+    cRed.style.top = '3px';
+    cRed.style.right = '3px';
+    cRed.style.width = '16px';
+    cRed.style.height = '16px';
+    cRed.style.backgroundColor = 'red';
+    cRed.style.borderRadius = '6px';
+    cRed.style.cursor = 'pointer';
+    w.appendChild(cRed);
+
+    var alertButton1 = document.createElement('button');
+    alertButton1.textContent = 'Alert 1';
+    alertButton1.style.position = 'absolute';
+    alertButton1.style.top = '60px';
+    alertButton1.style.left = '30px';
+    alertButton1.style.padding = '10px';
+    alertButton1.style.backgroundColor = 'blue';
+    alertButton1.style.color = 'white';
+    alertButton1.style.border = 'none';
+    alertButton1.style.borderRadius = '4px';
+    alertButton1.style.cursor = 'pointer';
+    o.appendChild(alertButton1);
+
+    var alertButton2 = document.createElement('button');
+    alertButton2.textContent = 'Alert 2';
+    alertButton2.style.position = 'absolute';
+    alertButton2.style.top = '60px';
+    alertButton2.style.left = '150px';
+    alertButton2.style.padding = '10px';
+    alertButton2.style.backgroundColor = 'green';
+    alertButton2.style.color = 'white';
+    alertButton2.style.border = 'none';
+    alertButton2.style.borderRadius = '4px';
+    alertButton2.style.cursor = 'pointer';
+    o.appendChild(alertButton2);
+
+    function moveWindow(e) {
+      var posX = e.clientX - offsetX;
+      var posY = e.clientY - offsetY;
+      o.style.left = posX + 'px';
+      o.style.top = posY + 'px';
+    }
+
+    var offsetX = 0;
+    var offsetY = 0;
+    var dragging = false;
+
+    w.addEventListener('mousedown', function(e) {
+      dragging = true;
+      offsetX = e.clientX - parseInt(o.style.left);
+      offsetY = e.clientY - parseInt(o.style.top);
+    });
+
+    window.addEventListener('mouseup', function() {
+      dragging = false;
+    });
+
+    window.addEventListener('mousemove', function(e) {
+      if (dragging) {
+        moveWindow(e);
+      }
+    });
+
+    alertButton1.addEventListener('click', function() {
+      alert('Button 1 clicked!');
+    });
+
+    alertButton2.addEventListener('click', function() {
+      alert('Button 2 clicked!');
+    });
+
+    cOrange.addEventListener('click', function() {
+      alert('Close button clicked!');
+      o.parentNode.removeChild(o);
+    });
+
+    cRed.addEventListener('click', function() {
+      alert('Minimize button clicked!');
+      o.style.opacity = '0';
+      o.style.transform = 'scale(0)';
+    });
+  }
+})();
