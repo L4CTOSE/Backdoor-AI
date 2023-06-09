c077617577bb62cd08a1b6838339cf95dc63d8c7
L4CTOSE <owner@zenlysocial.com>
2023-06-09 12:53:53 -0400

    Update bookmarklet.js
    Signed-off-by: L4CTOSE <owner@zenlysocial.com>

--- bookmarklet.js
+++ bookmarklet.js
@@ -8,21 +8,21 @@ javascript:(function() {
       aiName: '<div class="mwai-name-text">AI</div>',
       apiKey: '',
       clientId: 'fsbdffqy13f',
-      context: 'Converse as if you were an AI assistant. Be friendly, creative.',
+      context: '',
       embeddingsIndex: '',
       maxResults: 1,
-      maxTokens: 256,
+      maxTokens: 999999,
       messages: [
         { id: 'egtgr9ghdf', role: 'assistant', content: 'Hi! How can I help you?', who: 'AI: ' },
         { id: 'your-message-id', role: 'user', content: userInput, who: 'You: ' },
       ],
       model: 'gpt-3.5-turbo',
       newMessage: userInput,
-      prompt: 'Converse as if you were an AI assistant. Be friendly, creative.\n\nAI: Hi! How can I help you?\nUser: ' + userInput + '\nAI: ',
+      prompt: '' + userInput + '\nAI: ',
       service: 'openai',
       session: '64829136953f7',
       stop: '',
-      temperature: 0.8,
+      temperature: 0,
       userName: '<div class="mwai-name-text">You</div>'
     };
 
@@ -35,11 +35,9 @@ javascript:(function() {
           var response = JSON.parse(xhr.responseText);
           var answer = response.answer;
           alert(answer);
-        } else if (xhr.status === 500 || xhr.responseText.includes('Rate limit reached')) {
-          setTimeout(function() {
-            xhr.open('POST', apiUrl);
-            xhr.send(JSON.stringify(payload));
-          }, 500);
+        } else {
+          var errorMessage = "Contacting ChatGPT Failed... Try again: " + userInput;
+          alert(errorMessage);
         }
       }
     };
