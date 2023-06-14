9687017e087562abfea1d978dbd3c404d61a4db4
L4CTOSE <owner@zenlysocial.com>
2023-06-13 22:45:27 -0400

    Create replacement.js
    Signed-off-by: L4CTOSE <owner@zenlysocial.com>

---
+++ replacement.js
@@ -0,0 +1,18 @@
+javascript: (function() {
+  var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  var screenHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+  
+  var iframe = document.createElement('iframe');
+  iframe.style.position = 'fixed';
+  iframe.style.top = '0';
+  iframe.style.left = '0';
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+  iframe.style.border = 'none';
+  iframe.src = 'https://www.bing.com/chat';
+  
+  document.body.innerHTML = '';
+  document.body.appendChild(iframe);
+  
+  window.resizeTo(screenWidth, screenHeight);
+})();
